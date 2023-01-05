9b1f2cb4821f672580efb7be39fe4f9edfb5afc0
Jhonatan1022780 <rojasjhonatan324@gmail.com>
2023-01-04 23:10:01 -0500

    all the logic of the animations was passed to the customHook useMenu

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -1,44 +1,15 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
+import { useMenu } from '../../hooks/useMenu'
+import useStore from '../../store/store'
 import { Logo } from '../UI/Logo'
 import { Option } from './Option'
 import { Toggle } from './Toggle'
 
 export const LeftBar = () => {
-  const [showBar, setShowBar] = useState(null)
   const refLeftBar = useRef(null)
   const refBtnFloat = useRef(null)
-
-  const toggleBar = () => {
-    showBar === null ? setShowBar(false) : setShowBar(!showBar)
-  }
-
-  const toggleBarFloat = () => {
-    showBar === null ? setShowBar(false) : setShowBar(!showBar)
-    refLeftBar.current.classList.toggle('leftbar--hide')
-    refBtnFloat.current.classList.toggle('btn-float--hide')
-  }
-
-  useEffect(() => {
-    // si es la primera vez que se ejecuta este efecto no se ejecuta nada
-    if (showBar === null) return
-    const grid = document.querySelector('.grid')
-    if (showBar) {
-      refLeftBar.current.classList.remove('leftbar--hide')
-      grid.classList.remove('grid--expand')
-      grid.classList.add('grid--reduce')
-      refLeftBar.current.classList.add('leftbar--show')
-      refBtnFloat.current.classList.add('btn-float--hide')
-      refBtnFloat.current.classList.remove('btn-float--show')
-    } else {
-      refLeftBar.current.classList.add('leftbar--hide')
-      refLeftBar.current.classList.remove('leftbar--show')
-      refBtnFloat.current.classList.remove('btn-float--hide')
-      grid.classList.add('grid--expand')
-      grid.classList.remove('grid--reduce')
-      refBtnFloat.current.classList.add('btn-float--show')
-    }
-  }, [showBar])
-
+  const [toggleBar, toggleBarFloat] = useMenu(refLeftBar, refBtnFloat)
+  const boards = useStore((state) => state.board)
   return (
     <>
       <aside className="leftbar" ref={refLeftBar}>
@@ -48,9 +19,11 @@ export const LeftBar = () => {
             <h3>ALL BOARDS (3)</h3>
 
             <nav className="mt-1">
-              <Option value="Platform Launch" active />
-              <Option value="Marketing Plan" />
-              <Option value="Roadmap" />
+              {boards.boards?.map((board) => (
+                <div key={board.id}>
+                  <Option value={board.name} active={boards.active.id} />
+                </div>
+              ))}
               <Option value="+ Create New Board" action />
             </nav>
           </div>
